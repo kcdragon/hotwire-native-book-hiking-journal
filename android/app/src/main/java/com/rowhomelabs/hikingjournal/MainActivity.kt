c8a070dbb52d3eb023846f007b78c4cb8bb079a4
Mike Dalton <michaelcdalton@gmail.com>
2025-04-10 10:14:24 -0700

    Android app works

--- android/app/src/main/java/com/rowhomelabs/hikingjournal/MainActivity.kt
+++ android/app/src/main/java/com/rowhomelabs/hikingjournal/MainActivity.kt
@@ -1,20 +1,27 @@
-package com.rowhomelabs.hikingjournal
+package com.rowhomelabs.hikingjournal;
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import dev.hotwire.navigation.activities.HotwireActivity
+import dev.hotwire.navigation.navigator.NavigatorConfiguration
+import dev.hotwire.navigation.util.applyDefaultImeWindowInsets
 
-class MainActivity : AppCompatActivity() {
+const val baseURL = "http://10.0.2.2:3000"
+
+class MainActivity : HotwireActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        findViewById<View>(R.id.main).applyDefaultImeWindowInsets()
     }
+
+    override fun navigatorConfigurations(): List<NavigatorConfiguration> = listOf(
+        NavigatorConfiguration(
+            name = "main",
+            startLocation = "$baseURL/hikes",
+            navigatorHostId = R.id.main
+        )
+    )
 }
