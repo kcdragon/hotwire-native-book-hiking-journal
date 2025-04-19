e5bd0d56d35793daefdc66c56f3ece243df0144a
Mike Dalton <michaelcdalton@gmail.com>
2025-04-19 11:24:39 -0400

    Add Android Tab Bar

--- android/app/src/main/java/com/rowhomelabs/hikingjournal/activities/MainActivity.kt
+++ android/app/src/main/java/com/rowhomelabs/hikingjournal/activities/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.rowhomelabs.hikingjournal;
+package com.rowhomelabs.hikingjournal.activities;
 
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.rowhomelabs.hikingjournal.R
+import com.rowhomelabs.hikingjournal.models.Tab
 import dev.hotwire.core.config.Hotwire
 import dev.hotwire.core.turbo.config.PathConfiguration
 import dev.hotwire.navigation.activities.HotwireActivity
@@ -12,11 +15,21 @@ import dev.hotwire.navigation.util.applyDefaultImeWindowInsets
 const val baseURL = "http://10.0.2.2:3000"
 
 class MainActivity : HotwireActivity() {
+    private val tabs = Tab.all
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         findViewById<View>(R.id.main).applyDefaultImeWindowInsets()
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setOnItemSelectedListener { tab ->
+            val selectedTab = tabs.first { it.menuId == tab.itemId }
+            showTab(selectedTab)
+            true
+        }
+        showTab(tabs.first())
 
         Hotwire.loadPathConfiguration(
             context = this,
@@ -26,11 +39,18 @@ class MainActivity : HotwireActivity() {
         )
     }
 
-    override fun navigatorConfigurations(): List<NavigatorConfiguration> = listOf(
+    override fun navigatorConfigurations() = tabs.map { tab ->
         NavigatorConfiguration(
-            name = "main",
-            startLocation = "$baseURL/hikes",
-            navigatorHostId = R.id.main
+            name = tab.name,
+            startLocation = "$baseURL/${tab.path}",
+            navigatorHostId = tab.navigatorHostId
         )
-    )
+    }
+
+    private fun showTab(tab: Tab) {
+        tabs.forEach {
+            val view = findViewById<View>(it.navigatorHostId)
+            view.visibility = if (it == tab) View.VISIBLE else View.GONE
+        }
+    }
 }
